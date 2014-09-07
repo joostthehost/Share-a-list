845fa0d09e433b482508b53d2dcd1078bcf226b1
Joost <timmerman_joost@hotmail.com>
2014-09-07 17:50:33 +0200

    Fancy animations

--- app/src/main/java/com/joosttimmerman/share_a_list/lists_overview.java
+++ app/src/main/java/com/joosttimmerman/share_a_list/lists_overview.java
@@ -1,21 +1,57 @@
 package com.joosttimmerman.share_a_list;
 
+import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 
-public class lists_overview extends ActionBarActivity {
+public class lists_overview extends Activity {
 
-    //iets toevoeg
+    //declare globals;
+    TextView listTitle;
+    Animation animSlideDown, animSlideUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists_overview);
+
+        // load the animation
+        animSlideDown = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
+        animSlideUp = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_up);
+
+        //get the textview
+        listTitle = (TextView)findViewById(R.id.listTitle);
+
+        //set font
+        //Typeface roboto_thin = Typeface.createFromAsset(getAssets(),"Roboto-Thin.ttf");
+        //listTitle.setTypeface(roboto_thin);
+
+        //overridePendingTransition(R.anim.slide_right2left,R.anim.scale_down_darken);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //animate title slide
+        listTitle.startAnimation(animSlideDown);
+    }
+
+    @Override
+    public void onPause() {
+
+        //animate title slide
+        listTitle.startAnimation(animSlideUp);
+
+        super.onPause();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
